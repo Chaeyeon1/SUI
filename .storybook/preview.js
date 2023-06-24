b3800c666517b59f8082aac9e3ad0bb0f5108dab
chaeyeon1 <slide10245@naver.com>
2023-06-24 10:57:34 +0900

    #1 스토리북 다운로드

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,4 @@
 /** @type { import('@storybook/react').Preview } */
-import { useDarkMode } from "storybook-dark-mode";
-
-// export const decorators = [
-//   (Story: any) => {
-//     document.documentElement.setAttribute(
-//       "color-theme",
-//       useDarkMode() ? "dark" : "light"
-//     );
-
-//     return <Story />;
-//   },
-// ];
-
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
