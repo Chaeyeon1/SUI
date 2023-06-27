37f2d57e5a34870598e6643ef3395757c06938b6
suyeon <moa0823@naver.com>
2023-06-27 12:39:24 +0900

    #16 chip removable 클릭 시 사라지는 기능 구현

--- src/components/chip/Chip.tsx
+++ src/components/chip/Chip.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
 import clsx from 'clsx';
+import React, { useState } from 'react';
+
 interface ChipProps
   extends Omit<React.HTMLAttributes<HTMLDivElement>, 'size'> {
   variant?: 'contained' | 'outlined';
@@ -8,9 +9,10 @@ interface ChipProps
   color?: 'primary' | 'secondary';
   disabled?: boolean;
   text?: string;
+  open?: boolean;
 }
   
-  const baseStyles = 'font-medium rounded  h-8 rounded-full px-3 pt-1 fit-content';
+  const baseStyles = 'font-medium rounded  h-9 rounded-full px-3 pt-1 fit-content  ';
   
   const variantStyles = {
     contained: 'text-black',
@@ -22,7 +24,6 @@ interface ChipProps
   };
 
   const thumbnail = {
-
   };
   
   const disabledSyles = 'cursor-default pointer-events-none';
@@ -51,19 +52,30 @@ interface ChipProps
     thumbnail = false,
     text,
     children,
+    open = true,
     ...props
   }:ChipProps) {
     const chipClass = disabled
     ? clsx(className, baseStyles, disabledSyles)
     : clsx(className, baseStyles, colorStyles[variant][color]);
+
+    const [close, setClose] = useState(true);
   
     return (
-        <div className={chipClass} {...props}>
-            <div className='flex gap-3'>
-                {thumbnail && <div>thumbnail</div>} 
-                <div>{text&&<div>{text}</div>}</div>   
-                {removable && <div className="bg-white rounded-full text-black w-6 pl-2">x</div>}
+        <div>
+          {close ? (
+            <div className={chipClass} {...props}>
+              <div className='flex gap-2 pt-0.5'>
+                  {thumbnail && <div className="bg-white rounded-xl w-6 opacity-80 cursor-pointer"></div>} 
+                  <div>{text&&<div>{text}</div>}</div>
+                  {thumbnail && <div className="bg-white rounded-xl w-6 opacity-80 hidden cursor-pointer"></div>} 
+                  {removable && <div onClick={() => setClose(!close)} className="bg-white rounded-xl text-center
+                  text-black w-6 opacity-80 cursor-pointer">x</div>}
+              </div>
             </div>
+            ):(
+              <div className="hidden"></div>
+            )}
         </div>
     );
   }
