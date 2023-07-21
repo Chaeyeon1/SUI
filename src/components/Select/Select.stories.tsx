4bb842bf733a763263d61ca718a66a1b05c58585
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-07-21 23:10:38 +0900

    #35 storybook layout에 맞춰서 정리

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Select } from './Select';
 import { SelectProps } from './Select.type';
+import { ComponentLayout, Layout } from '../Layout';
 
 export default {
   title: 'Components/Select',
@@ -17,3 +18,30 @@ export const Preview = (args: SelectProps) => (
     {...args}
   />
 );
+
+export const Color = () => {
+  return (
+    <Layout title="select">
+      <ComponentLayout title="primary">
+        <Select
+          color="primary"
+          options={[
+            { id: 0, content: 'Python' },
+            { id: 1, content: 'Java' },
+            { id: 2, content: 'JavaScript' },
+          ]}
+        />
+      </ComponentLayout>
+      <ComponentLayout title="secondary">
+        <Select
+          color="secondary"
+          options={[
+            { id: 0, content: 'Python' },
+            { id: 1, content: 'Java' },
+            { id: 2, content: 'JavaScript' },
+          ]}
+        />
+      </ComponentLayout>
+    </Layout>
+  );
+};
