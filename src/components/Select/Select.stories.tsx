cc83dbd78bce08a6edf2dff7e31535c82fde8405
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-07-22 23:02:47 +0900

    #35 storybook option list 변경

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -20,27 +20,19 @@ export const Preview = (args: SelectProps) => (
 );
 
 export const Color = () => {
+  const options = [
+    { id: 0, content: 'Suyeon' },
+    { id: 1, content: 'Chaeyeon' },
+    { id: 2, content: 'Sui' },
+  ];
+
   return (
     <Layout title="select">
       <ComponentLayout title="primary">
-        <Select
-          color="primary"
-          options={[
-            { id: 0, content: 'Python' },
-            { id: 1, content: 'Java' },
-            { id: 2, content: 'JavaScript' },
-          ]}
-        />
+        <Select color="primary" options={options} />
       </ComponentLayout>
       <ComponentLayout title="secondary">
-        <Select
-          color="secondary"
-          options={[
-            { id: 0, content: 'Python' },
-            { id: 1, content: 'Java' },
-            { id: 2, content: 'JavaScript' },
-          ]}
-        />
+        <Select color="secondary" options={options} />
       </ComponentLayout>
     </Layout>
   );
